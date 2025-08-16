b8a177326d3b970f3262d22917f57165fb362781
“VishnuSBhatDS” <vishnu.bhat@dealshare.in>
2025-08-17 00:15:06 +0530

    added wakeup endpoint

--- server.js
+++ server.js
@@ -1,25 +1,36 @@
 /* eslint-disable no-undef */
-const cron = require('node-cron');
-const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json'); 
+const cron = require("node-cron");
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
+const express = require("express");
+const app = express();
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 const db = admin.firestore();
 
-const COLLECTION = 'chat-updates'; 
+const COLLECTION = "chat-updates";
 
-cron.schedule('* * * * *', async () => {
-  console.log('Running Firestore deletion task:', new Date());
+app.use(express.json());
+app.get("/wakeup", (req, res) => {
+  res.status(200).send("Cron-Service is active!");
+});
+
+app.listen(() => {
+  console.log("Listening on port 8080.");
+}, 8080);
+cron.schedule("* * * * *", async () => {
+  console.log("Running Firestore deletion task:", new Date());
   try {
     const now = Date.now();
-    const snapshot = await db.collection(COLLECTION)
-      .where('expireAt', '<', now)
+    const snapshot = await db
+      .collection(COLLECTION)
+      .where("expireAt", "<", now)
       .get();
 
     const batch = db.batch();
-    snapshot.forEach(doc => {
+    snapshot.forEach((doc) => {
       batch.delete(doc.ref);
       console.log(`Queued deletion: ${doc.id}`);
     });
@@ -28,15 +39,14 @@ cron.schedule('* * * * *', async () => {
     console.log(`Deleted ${snapshot.size} expired documents.`);
 
     await db.collection(COLLECTION).add({
-        type: 'deletion-log',
-        timestamp: new Date(),
-        message: `Deleted cron ran.`
-      });
-      console.log('Logged deletion operation in collection.');
+      type: "deletion-log",
+      timestamp: new Date(),
+      message: `Deleted cron ran.`,
+    });
+    console.log("Logged deletion operation in collection.");
   } catch (err) {
-    console.error('Error deleting expired docs:', err);
+    console.error("Error deleting expired docs:", err);
   }
 });
 
-console.log('Firestore deletion cron started!');
-
+console.log("Firestore deletion cron started!");
